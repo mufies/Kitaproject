da7a6ccedbece9c9f9928805e05217ba51d05c27
mufies <kixsuna1@gmail.com>
2026-02-26 08:41:29 +0700

    update migration

--- kita/Infrastructure/Migrations/20260225035846_UpdateAssetUrlsToHttps.cs
+++ kita/Infrastructure/Migrations/20260225035846_UpdateAssetUrlsToHttps.cs
@@ -27,8 +27,6 @@ namespace Infrastructure.Migrations
                 UPDATE ""Albums"" SET ""ImageUrl"" = REPLACE(""ImageUrl"", 'http://100.98.82.105:5064/Assets', 'https://api.kiseki.id.vn/Assets') WHERE ""ImageUrl"" LIKE 'http://100.98.82.105:5064/Assets%';
                 UPDATE ""Servers"" SET ""IconUrl"" = REPLACE(""IconUrl"", 'http://100.98.82.105:5064/Assets', 'https://api.kiseki.id.vn/Assets') WHERE ""IconUrl"" LIKE 'http://100.98.82.105:5064/Assets%';
                 UPDATE ""Messages"" SET ""ImageUrl"" = REPLACE(""ImageUrl"", 'http://100.98.82.105:5064/Assets', 'https://api.kiseki.id.vn/Assets') WHERE ""ImageUrl"" LIKE 'http://100.98.82.105:5064/Assets%';
-                UPDATE ""Messages"" SET ""VideoUrl"" = REPLACE(""VideoUrl"", 'http://100.98.82.105:5064/Assets', 'https://api.kiseki.id.vn/Assets') WHERE ""VideoUrl"" LIKE 'http://100.98.82.105:5064/Assets%';
-                UPDATE ""Messages"" SET ""FileUrl"" = REPLACE(""FileUrl"", 'http://100.98.82.105:5064/Assets', 'https://api.kiseki.id.vn/Assets') WHERE ""FileUrl"" LIKE 'http://100.98.82.105:5064/Assets%';
             ");
         }
 
